4a4e7fa4e737c8809208e9095a47783ef25b6f05
NatuEspinoza <natalia.utemdcv@gmail.com>
2017-12-14 11:06:05 -0300

    Commit final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,148 +1,191 @@
 $(document).ready(function() {
-/**
- * Función de la vista splash inicial
- *
- */
-$().ready(function() {
-  $('.inicial').hide();
-  $("#splash").fadeIn(500).fadeOut(5000, show);
+  /**
+   * Función de la vista splash inicial
+   *
+   */
+  $().ready(function() {
+    $('.inicial').hide();
+    $("#splash").fadeIn(500).fadeOut(5000, show);
 
-  function show() {
-    $('.inicial').show();
-  }
-});
-
-/**
- * Función para avanzar a la siguiente sección
- *
- */
-$('#signUp').click(function() {
-  $('#form').show();
-  $('#inicial').hide();
-})
-
-/**
- * Función para ir a la sección anterior
- */
-$('#return').click(function() {
-  $('#inicial').show();
-  $('#form').hide();
-})
-
-
-/**
- * Función del input de registro
- *
- */
-$('.search-panel .dropdown-menu').find('a').click(function(e) {
-  e.preventDefault();
-  var param = $(this).attr("href").replace("#", "");
-  var concept = $(this).text();
-  $('.search-panel span#search_concept').text(concept);
-  $('.input-group #search_param').val(param);
-});
-
-/**
- * Función para desabilitar y habilitar boton
- */
-$('#number').keyup(function() {
-  var value = $('#number').val().length;
-  if (value === 10) {
-    $('#btn-next').removeClass('disabled')
-  }
-  if (value > 10 || $('#number').val() == '') {
-    $('#btn-next').addClass('disabled');
-  }
-  if (isNaN($('#number').val())){
-    $('#alert').html('<h3 style="color: #ff00bf;">Favor ingresar sólo números!</h3>');
-    $('#btn-next').addClass('disabled');
-  }  else{
-      $('#alert').text('');
-  }
-  return false;
-});
-/**
- * Alerta con código aleatorio
- */
-$(function() {
-  $('#btn-next').click(function(result) {
-    $('#number').val('');
-    $('#btn-next').addClass('disabled');
-    $('#btn-ok').show();
-    var result = 1; //test the output message
-    var success = '<div class="alert alert-success fade in" role="alert">'
-    var success = '<div class="alert alert-success fade in" role="alert">' +
-      '<a class="close" data-dismiss="alert">&times</a>' +
-      '<strong><span class="glyphicon glyphicon-ok"></span> Tu código es: ' +
-      '<span style="font-size: 2em">LAB-</span><span style="font-size: 2em" id="numero-aleatorio" style="text-align:center;"> </span>' +
-    '</strong>';
-    var output = success;
-    $('#alert-dialogue').append(output);
-
-    numeroAleatorio(1, 100);
+    function show() {
+      $('.inicial').show();
+    }
   });
 
-  numeroAleatorio(1, 100);
+  /**
+   * Función para avanzar a la siguiente sección
+   *
+   */
+  $('#signUp').click(function() {
+    $('#form').show();
+    $('#inicial').hide();
+    $('#verify').hide();
+    $('#form-signUp').hide();
+    $('#final').hide();
+  })
 
-  function numeroAleatorio(min, max) {
-    var num = Math.round(Math.random() * (max - min) + min);
-    $('#numero-aleatorio').text(num);
-  }
-});
 
-/**
- * Función para avanzar a la siguiente sección
- *
- */
-$('#btn-ok').click(function() {
-  $('#form').hide();
-  $('#verify').show();
-})
+  /**
+   * Función para ir a la sección anterior
+   */
+  $('#return').click(function() {
+    $('#inicial').show();
+    $('#form').hide();
+    $('#verify').hide();
+    $('#form-signUp').hide();
+    $('#final').hide();
+  })
 
-/**
- * Función para ir a la sección anterior
- */
-$('#return-2').click(function(){
-  $('#form').show();
-  $('#verify').hide();
-})
 
-/**
- * Función para habilitar botón de la sección de verify
- */
- /**
-  * Función para desabilitar y habilitar boton
-  */
- $('#codigo').keyup(function() {
-   var value = $('#codigo').val().length;
-   if (value <=3) {
-     $('#btn-next2').removeClass('disabled')
-   }
-   if (value >3 || $('#codigo').val() == ''){
-     $('#btn-next2').addClass('disabled');
-   }
-   if (isNaN($('#codigo').val())){
-     $('#alert2').html('<h3 style="color: #ff00bf;">Favor ingresar sólo números!</h3>');
-     $('#btn-next2').addClass('disabled');
-   }  else{
-       $('#alert2').text('');
-   }
-   return false;
- });
+  /**
+   * Función del input de registro
+   *
+   */
+  $('.search-panel .dropdown-menu').find('a').click(function(e) {
+    e.preventDefault();
+    var param = $(this).attr("href").replace("#", "");
+    var concept = $(this).text();
+    $('.search-panel span#search_concept').text(concept);
+    $('.input-group #search_param').val(param);
+  });
 
- /**
-  * Función para botón NEXT para avanzar a la sección del formulario sign up
-  */
- $('#btn-next2').click(function(){
-   $('#verify').hide();
-   $('#form-signUp').show();
- })
- /**
-  * Función para regresar a la sección de Verify
-  */
-  $('#return-3').click(function(){
+  /**
+   * Función para desabilitar y habilitar boton
+   */
+  $('#number').keyup(function() {
+    var value = $('#number').val().length;
+    if (value === 10) {
+      $('#btn-next').removeClass('disabled')
+    }
+    if (value > 10 || $('#number').val() == '') {
+      $('#btn-next').addClass('disabled');
+    }
+    if (isNaN($('#number').val())) {
+      $('#alert').html('<h3 style="color: #ff00bf;">Favor ingresar sólo números!</h3>');
+      $('#btn-next').addClass('disabled');
+    } else {
+      $('#alert').text('');
+    }
+    return false;
+  });
+  /**
+   * Alerta con código aleatorio
+   */
+  $(function() {
+    $('#btn-next').click(function(result) {
+      $('#number').val('');
+      $('#btn-next').addClass('disabled');
+      $('#btn-ok').show();
+      var result = 1; //test the output message
+      var success = '<div class="alert alert-success fade in" role="alert">'
+      var success = '<div class="alert alert-success fade in" role="alert">' +
+        '<a class="close" data-dismiss="alert">&times</a>' +
+        '<strong><span class="glyphicon glyphicon-ok"></span> Tu código es: ' +
+        '<span style="font-size: 2em">LAB-</span><span style="font-size: 2em" id="numero-aleatorio" style="text-align:center;"> </span>' +
+        '</strong>';
+      var output = success;
+      $('#alert-dialogue').append(output);
+
+      numeroAleatorio(1, 100);
+    });
+
+    numeroAleatorio(1, 100);
+
+    function numeroAleatorio(min, max) {
+      var num = Math.round(Math.random() * (max - min) + min);
+      $('#numero-aleatorio').text(num);
+    }
+  });
+
+  /**
+   * Función para avanzar a la siguiente sección
+   *
+   */
+  $('#btn-ok').click(function() {
+    $('#form').hide();
     $('#verify').show();
     $('#form-signUp').hide();
+    $('#final').hide();
+    $('#inicial').hide();
   })
+
+  /**
+   * Función para ir a la sección anterior
+   */
+  $('#return-2').click(function() {
+    $('#form').show();
+    $('#verify').hide();
+    $('#form-signUp').hide();
+    $('#final').hide();
+    $('#inicial').hide();
+  })
+
+  /**
+   * Función para habilitar botón de la sección de verify
+   */
+  /**
+   * Función para desabilitar y habilitar boton
+   */
+  $('#codigo').keyup(function() {
+    var value = $('#codigo').val().length;
+    if (value <= 3) {
+      $('#btn-next2').removeClass('disabled')
+    }
+    if (value > 3 || $('#codigo').val() == '') {
+      $('#btn-next2').addClass('disabled');
+    }
+    if (isNaN($('#codigo').val())) {
+      $('#alert2').html('<h3 style="color: #ff00bf;">Favor ingresar sólo números!</h3>');
+      $('#btn-next2').addClass('disabled');
+    } else {
+      $('#alert2').text('');
+    }
+    return false;
+  });
+
+  /**
+   * Función para botón NEXT para avanzar a la sección del formulario sign up
+   */
+  $('#btn-next2').click(function() {
+    $('#verify').hide();
+    $('#form-signUp').show();
+    $('#form').hide();
+    $('#inicial').hide();
+    $('#inicial').hide();
+  })
+  /**
+   * Función para regresar a la sección de Verify
+   */
+  $('#return-3').click(function() {
+    $('#verify').show();
+    $('#form-signUp').hide();
+    $('#form').hide();
+    $('#inicial').hide();
+    $('#inicial').hide();
+  })
+  /**
+   * función para habilitar y desabilitar botón de enviar el formulario
+   */
+  $('#user, #email').keyup(function() {
+    $('#btn-next3').removeClass('disabled');
+  })
+
+  /**
+   * Función del checkbox para aceptar condiciones
+   */
+  $("#btn-next3").on("click", function() {
+    var condiciones = $("#checkbox").is(":checked");
+    if (!condiciones) {
+      alert("Debe aceptar las condiciones");
+      event.preventDefault();
+    } else {
+      $('#final').show();
+      $('#form-signUp').hide();
+      $('#verify').hide();
+      $('#form').hide();
+      $('#inicial').hide();
+    }
+  });
+
 
 });
